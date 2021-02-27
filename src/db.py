b9a8cc9cdff56346038c3dc62258b2ffbdd5abba
Carlos Requena López <Carlos.Requena.Lopez@ulb.be>
2021-02-27 21:53:26 +0100

    fix: add BASE_PATH to open .sql

--- src/db.py
+++ src/db.py
@@ -2,16 +2,17 @@ import os
 import sqlalchemy
 from .log.core import logger
 
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 engine = sqlalchemy.create_engine(os.environ["DATABASE_URL"])
 
 
 def build_tables():
-    with open("sql/player_trends.sql", "r") as sql_file:
+    with open(f"{BASE_PATH}/sql/player_trends.sql", "r") as sql_file:
         query = sqlalchemy.text(sql_file.read())
         logger.info("Writing player_trends to db...")
         engine.execute(query)
 
-    with open("sql/my_team.sql", "r") as sql_file:
+    with open(f"{BASE_PATH}/sql/my_team.sql", "r") as sql_file:
         query = sqlalchemy.text(sql_file.read().replace("<<MANAGER_ID>>", os.environ["MANAGER_ID"]))
         logger.info("Cleaning myteam table...")
         engine.execute(query)
